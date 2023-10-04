76610e7dce6f94580af773b863d063d39366c703
BuilderZac <zacharyl2005@gmail.com>
2023-10-04 11:48:03 -0400

    added eyeliner

--- init.lua
+++ init.lua
@@ -107,6 +107,15 @@ require('lazy').setup({
     },
   },
 
+  { -- Add eyeliner
+    'jinh0/eyeliner.nvim',
+      config = function()
+      require'eyeliner'.setup {
+        highlight_on_key = true
+      }
+    end
+  },
+
   { -- Add color schemes
     'rafi/awesome-vim-colorschemes'
   },
@@ -474,6 +483,18 @@ local servers = {
     },
   },
 }
+
+-- Setup Eyeliner
+--vim.api.nvim_set_hl(0, 'EyelinerPrimary', { bold = true, underline = true })
+--vim.api.nvim_set_hl(0, 'EyelinerSecondary', { underline = true })
+vim.api.nvim_set_hl(0, 'EyelinerPrimary', { fg='#f43636', bold = true, underline = true })
+vim.api.nvim_set_hl(0, 'EyelinerSecondary', { fg='#f4369c', underline = true })
+--vim.api.nvim_create_autocmd('ColorScheme', {
+--  pattern = '*',
+--  callback = function()
+--    vim.api.nvim_set_hl(0, 'EyelinerPrimary', { bold = true, underline = true })
+--  end,
+--})
 
 -- Setup Toggle terminal
 require("toggleterm").setup{
