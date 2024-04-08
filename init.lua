1f29eb69a1089aa81a667969d8899577324adf70
BuilderZac <zacharyl2005@gmail.com>
2024-04-08 11:13:47 -0400

    removed pylint

--- init.lua
+++ init.lua
@@ -193,6 +193,34 @@ require('lazy').setup({
     'preservim/nerdtree'
   },
 
+  { -- Add nvim-lint
+    "mfussenegger/nvim-lint",
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    config = function()
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+  	java = { "checkstyle" },
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>l", function()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
+    end,
+  },
+
   { -- Add felin
     "freddiehaddad/feline.nvim",
     config = function()
