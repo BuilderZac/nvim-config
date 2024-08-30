960394b28cbedbc935d58419a97c1bf101d421d4
cmdr <zacharyl2005@gmail.com>
2024-08-29 23:30:49 -0400

    made lsp binds the old ones

--- init.lua
+++ init.lua
@@ -12,6 +12,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
 	end,
 	desc = "Disable New Line Comment",
 })
+
+-- Set indent to three spaces
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.tabstop = 3
+vim.o.shiftwidth = 3
 -- c2 end
 
 -- Set <space> as the leader key
@@ -36,12 +42,6 @@ vim.opt.number = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
-
--- Set indent to three spaces
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.tabstop = 3
-vim.o.shiftwidth = 3
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
@@ -713,9 +713,9 @@ require("lazy").setup({
 				-- No, but seriously. Please read `:help ins-completion`, it is really good!
 				mapping = cmp.mapping.preset.insert({
 					-- Select the [n]ext item
-					["<C-j>"] = cmp.mapping.select_next_item(), -- modded
+					--["<C-j>"] = cmp.mapping.select_next_item(), -- modded
 					-- Select the [p]revious item
-					["<C-k>"] = cmp.mapping.select_prev_item(), -- modded
+					--["<C-k>"] = cmp.mapping.select_prev_item(), -- modded
 
 					-- Scroll the documentation window [b]ack / [f]orward
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -728,14 +728,14 @@ require("lazy").setup({
 
 					-- If you prefer more traditional completion keymaps,
 					-- you can uncomment the following lines
-					--["<CR>"] = cmp.mapping.confirm({ select = true }),
-					--['<Tab>'] = cmp.mapping.select_next_item(),
-					--['<S-Tab>'] = cmp.mapping.select_prev_item(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
 
 					-- Manually trigger a completion from nvim-cmp.
 					--  Generally you don't need this, because nvim-cmp will display
 					--  completions whenever it has completion options available.
-					["<C-Space>"] = cmp.mapping.complete({}),
+					-- ["<C-Space>"] = cmp.mapping.complete({}),
 
 					-- Think of <c-l> as moving to the right of your snippet expansion.
 					--  So if you have a snippet that's like:
