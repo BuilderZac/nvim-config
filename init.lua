cb2bbd939144d5d8c82f3fb4bee1b9430b900543
BuilderZac <zacharyl2005@gmail.com>
2026-09-08 17:50:08 -0400

    added YAML to defualt treesitter for codecompanion

--- init.lua
+++ init.lua
@@ -1055,7 +1055,7 @@ do
 
 	-- Ensure basic parsers are installed
 	local parsers =
-		{ "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc"}
+		{ "bash", "c", "diff", "html", "lua", "yaml", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc"}
 	require("nvim-treesitter").install(parsers)
 
 	---@param buf integer
